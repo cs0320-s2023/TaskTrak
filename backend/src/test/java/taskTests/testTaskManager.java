e9d192a7381d0ebc760053211aa3731a3e8e863b
zleontiou <zachary_leontiou@brown.edu>
2023-05-01 12:35:47 -0400

    working through algorithim

---
+++ backend/src/test/java/taskTests/testTaskManager.java
@@ -0,0 +1,110 @@
+package taskTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Algorithim.TaskManager;
+import Enums.Rating;
+import Items.Task;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+class TaskManagerTest {
+  private TaskManager tm;
+  private Task task1;
+  private Task task2;
+
+  @BeforeEach
+  public void setUp() {
+    tm = new TaskManager();
+    task1 = new Task("Task 1", "Notes 1", Rating.HIGH, Rating.LOW, Duration.ofHours(1), LocalDateTime.now().plusDays(1), false);
+    task2 = new Task("Task 2", "Notes 2", Rating.LOW, Rating.LOW, Duration.ofMinutes(30), LocalDateTime.now().plusDays(2), false);
+    //task3 = new Task("Task 3", "Notes 3", Rating.LOW, Rating.LOW, Duration.ofMinutes(30),
+        LocalDateTime.now().plusDays(2), false);
+  }
+
+
+  @Test
+  void testAddTask() {
+
+    tm.addTask(task1);
+    tm.addTask(task2);
+
+    assertEquals(2, tm.getTaskMap().size());
+    assertEquals(task1, tm.getTaskMap().get("Task 1"));
+    assertEquals(task2, tm.getTaskMap().get("Task 2"));
+  }
+
+  @Test
+  void testRemoveTask() {
+    TaskManager tm = new TaskManager();
+    Task task1 = new Task("Task 1", "Notes 1", Rating.HIGH, Rating.LOW, Duration.ofHours(1),
+        LocalDateTime.now().plusDays(1), false);
+    Task task2 = new Task("Task 2", "Notes 2", Rating.LOW, Rating.LOW, Duration.ofMinutes(30),
+        LocalDateTime.now().plusDays(2), false);
+
+    assertEquals(0, tm.getTaskMap().size());
+    tm.addTask(task1);
+    tm.addTask(task2);
+    assertEquals(2, tm.getTaskMap().size());
+    tm.removeTask("Task 1");
+    assertEquals(1, tm.getTaskMap().size());
+    tm.removeTask("Task 2");
+    assertEquals(0, tm.getTaskMap().size());
+  }
+
+
+  @Test
+  public void testAddTask_NullTask() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.addTask(null));
+    assertEquals("Task cannot be null", exception.getMessage());
+  }
+
+
+  @Test
+  public void testAddTask_TaskAlreadyExists() {
+    tm.addTask(task1);
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.addTask(task1));
+    assertEquals("Task with name 'Task 1' already exists", exception.getMessage());
+  }
+
+
+  @Test
+  public void testRemoveTask_NullName() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.removeTask(null));
+    assertEquals("Name cannot be null", exception.getMessage());
+  }
+
+
+  @Test
+  public void testRemoveTask_NonExistingTask() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.removeTask("Task 1"));
+    assertEquals("Task with name 'Task 1' does not exist", exception.getMessage());
+  }
+
+
+  @Test
+  public void testGetTask_NullName() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.getTask(null));
+    assertEquals("Name cannot be null", exception.getMessage());
+  }
+
+  @Test
+  public void testGetTask_NonExistingTask() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> tm.getTask("Task1"));
+    assertEquals("Task with name 'Task1' does not exist", exception.getMessage());
+  }
+
+
+
+
+}
