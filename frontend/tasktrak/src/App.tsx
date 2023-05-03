3a6fab09a56ad5ec1f60da5c66baac20eb71f047
Ricardo Ornelas <91759857+rornela@users.noreply.github.com>
2023-05-03 15:18:49 -0400

    frontend: fixed view button

--- frontend/tasktrak/src/App.tsx
+++ frontend/tasktrak/src/App.tsx
@@ -1,4 +1,4 @@
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 import {
   Scheduler,
   DayView,
@@ -8,103 +8,109 @@ import {
   DateNavigator,
   TodayButton,
   WeekView,
-} from '@devexpress/dx-react-scheduler-material-ui';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import { useState } from 'react';
-import './App.css';
-import { Button, ButtonTypeMap, ExtendButtonBase, Grid, Menu, MenuItem } from '@mui/material';
-import MonthlyCalendar from './MonthlyCalendar';
-import DailyCalendar from './DailyCalendar';
-import WeeklyCalendar from './WeeklyCalendar'
-import React from 'react';
-import { sampleCalendarItems } from './CalendarItem';
-import { CalendarItem } from './CalendarItem';
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import { useState } from "react";
+import "./App.css";
+import {
+  Button,
+  ButtonTypeMap,
+  ExtendButtonBase,
+  Grid,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MonthlyCalendar from "./MonthlyCalendar";
+import DailyCalendar from "./DailyCalendar";
+import WeeklyCalendar from "./WeeklyCalendar";
+import React from "react";
+import { sampleCalendarItems } from "./CalendarItem";
+import { CalendarItem } from "./CalendarItem";
 
 function App() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [viewMode, setViewMode] = useState<'month' | 'week'>('month');
+  const [viewMode, setViewMode] = useState<"month" | "week">("month");
+  function toggleViewMode() {
+    setViewMode(viewMode === "month" ? "week" : "month");
+  }
 
   // could be updated with default value of CalendarItems[] if i move it to its own file ?
-  const [calendarItems, setCalendarItems] = useState(sampleCalendarItems)
+  const [calendarItems, setCalendarItems] = useState(sampleCalendarItems);
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-        // console.log(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    // console.log(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-  const calendarViewMenu: React.ReactNode[] = ([
+  const calendarViewMenu: React.ReactNode[] = [
     <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
+      id="basic-button"
+      aria-controls={open ? "basic-menu" : undefined}
+      aria-haspopup="true"
+      aria-expanded={open ? "true" : undefined}
+      onClick={toggleViewMode}
     >
-        testing button here
+      {viewMode === "month" ? "Switch to Week View" : "Switch to Month View"}
     </Button>,
     <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-            'aria-labelledby': 'basic-button',
-        }}
+      id="basic-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClose}
+      MenuListProps={{
+        "aria-labelledby": "basic-button",
+      }}
     >
-        <MenuItem onClick={handleClose}>Month View</MenuItem>
-        <MenuItem onClick={handleClose}>Week View</MenuItem>
-        <MenuItem onClick={handleClose}>Accessibility View</MenuItem>
-    </Menu>
-  ])
+      <MenuItem onClick={handleClose}>Month View</MenuItem>
+      <MenuItem onClick={handleClose}>Week View</MenuItem>
+      <MenuItem onClick={handleClose}>Accessibility View</MenuItem>
+    </Menu>,
+  ];
 
-  function toggleViewMode() {
-    setViewMode(viewMode === 'month' ? 'week' : 'month');
-  }
-return (
-  <Grid
-    container
-    spacing={6}
-    className="calendars"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Grid item xs={12}>
-      <Button onClick={toggleViewMode}>
-        {viewMode === 'month' ? 'Switch to Week View' : 'Switch to Month View'}
-      </Button>
-    </Grid>
-    <Grid item xs={6}>
-      {viewMode === 'month' ? (
-        <MonthlyCalendar
+  return (
+    <Grid
+      container
+      spacing={6}
+      className="calendars"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <Button onClick={handleClick}>Accessibility Mode</Button>
+      </Grid>
+      <Grid item xs={6}>
+        {viewMode === "month" ? (
+          <MonthlyCalendar
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            calendarItems={calendarItems}
+            setCalendarItems={setCalendarItems}
+            calendarViewMenu={calendarViewMenu}
+          />
+        ) : (
+          <WeeklyCalendar
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            calendarItems={calendarItems}
+            setCalendarItems={setCalendarItems}
+            calendarViewMenu={calendarViewMenu}
+          />
+        )}
+      </Grid>
+      <Grid item xs={4}>
+        <DailyCalendar
           currentDate={currentDate}
-          setCurrentDate={setCurrentDate}
           calendarItems={calendarItems}
           setCalendarItems={setCalendarItems}
-          calendarViewMenu={calendarViewMenu}
         />
-      ) : (
-        <WeeklyCalendar
-          currentDate={currentDate}
-          setCurrentDate={setCurrentDate}
-          calendarItems={calendarItems}
-          setCalendarItems={setCalendarItems}
-        />
-      )}
+      </Grid>
     </Grid>
-    <Grid item xs={4}>
-      <DailyCalendar
-        currentDate={currentDate}
-        calendarItems={calendarItems}
-        setCalendarItems={setCalendarItems}
-      />
-    </Grid>
-  </Grid>
-);
+  );
 }
 
 export default App;
