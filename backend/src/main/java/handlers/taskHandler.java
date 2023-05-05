8a2b084024428603bdd60df96914597f2208fdd3
zleontiou <zachary_leontiou@brown.edu>
2023-05-05 18:22:28 -0400

    pushing

--- backend/src/main/java/handlers/taskHandler.java
+++ backend/src/main/java/handlers/taskHandler.java
@@ -6,22 +6,28 @@ import Enums.Rating;
 import Items.Calendar;
 import Items.Day;
 import Items.Task;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import com.google.gson.Gson;
+
 
 public class taskHandler implements Route {
+
   private TaskManager userTaskManager;
   private Calendar userCalendar;
 
-  public taskHandler(TaskManager userTaskManager, Calendar userCalendar){}
+  public taskHandler(TaskManager userTaskManager, Calendar userCalendar) {
+  }
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
@@ -31,7 +37,6 @@ public class taskHandler implements Route {
     String duration = request.queryParams("duration");
     String dueDate = request.queryParams("dueDate");
     String isComplete = request.queryParams("isComplete");
-
 
     try {
 
@@ -56,7 +61,6 @@ public class taskHandler implements Route {
       // add Task to the task manager
       userTaskManager.addTask(task);
 
-
       LocalDate todaysDate = LocalDate.now();
       Day todaysSchedule = userCalendar.getSchedule(todaysDate); // Day object for the current day
       ArrayList<int[]> todaysFreeTime = todaysSchedule.findAvailableTimeRanges();
@@ -65,24 +69,33 @@ public class taskHandler implements Route {
       userTaskManager.suggestionHelper(task, todaysFreeTime);
 
       //the timeSuggestions for the just task that was added
-      ArrayList<Time[]> taskTimeSuggestions =  task.getTimeSuggestions();
+      ArrayList<Time[]> taskTimeSuggestions = task.getTimeSuggestions();
+      Gson gson = new Gson();
+      String jsonTaskTimeSuggestions = gson.toJson(taskTimeSuggestions);
+      response.type("timeSuggestions/json");
+
+      return taskTimeSuggestions;
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+    } catch (NumberFormatException e) {
+      // handle error for invalid number format
+      response.status(400); // Bad Request
+      return "Invalid input format for priority or duration";
+    } catch (DateTimeParseException e) {
+      // handle error for invalid date format
+      response.status(400); // Bad Request
+      return "Invalid input format for dueDate";
+    } catch (UnsupportedEncodingException e) {
+      // handle error for unsupported encoding
+      response.status(500); // Internal Server Error
+      return "Failed to decode input";
+    } catch (Exception e) {
+      // handle generic error
+      response.status(500); // Internal Server Error
+      return "Error occurred while processing request";
     }
-
   }
-
 
 }
