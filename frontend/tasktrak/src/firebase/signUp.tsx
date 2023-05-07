a8c6479792111e0491f1e71b50a40f211cd6976c
Ricardo Ornelas <91759857+rornela@users.noreply.github.com>
2023-05-07 18:46:06 -0400

    Update signUp.tsx

--- frontend/tasktrak/src/firebase/signUp.tsx
+++ frontend/tasktrak/src/firebase/signUp.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import { auth } from "./config";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { TextField, Button } from "@mui/material";
-
-// const auth = getAuth();
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
-    console.log(email);
     e.preventDefault();
     if (password !== confirmPassword) {
       console.error("Passwords don't match");
@@ -25,33 +23,38 @@ const SignUp = () => {
   };
 
   return (
-    <form onSubmit={handleSignup}>
-      <TextField
-        autoFocus
-        id="email sign-up"
-        type="email"
-        label="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        autoFocus
-        id="password sign-up"
-        type="password"
-        label="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <TextField
-        autoFocus
-        id="confirm-password"
-        type="password"
-        label="Confirm Password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <Button type="submit">Signup</Button>
-    </form>
+    <>
+      {!showForm && <Button onClick={() => setShowForm(true)}>Sign Up</Button>}
+      {showForm && (
+        <form onSubmit={handleSignup}>
+          <TextField
+            autoFocus
+            id="email sign-up"
+            type="email"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            id="password sign-up"
+            type="password"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            id="confirm-password"
+            type="password"
+            label="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Button type="submit">Signup</Button>
+        </form>
+      )}
+    </>
   );
 };
 
